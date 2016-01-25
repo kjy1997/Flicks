d5295ecb88d6a17c5a684b052895b70b67cdd25e
Angel Kou <kouj@purdue.edu>
2016-01-24 22:10:28 -0500

    Final Submission

--- Flicks/MovieViewController.swift
+++ Flicks/MovieViewController.swift
@@ -1,4 +1,3 @@
-//
 //  MovieViewController.swift
 //  Flicks
 //
@@ -8,30 +7,27 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
 class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate {
     @IBOutlet var tableView: UITableView!
     var refreshControl: UIRefreshControl!
-    var movies: [NSDictionary]?
-    var filteredMovies: [NSDictionary]?
+    var movies: NSMutableArray?
+    var filteredMovies = []
     
-    @IBOutlet var loadingAcitivity: UIActivityIndicatorView!
+    @IBOutlet var networkView: UIView!
+    @IBOutlet var searchbar: UISearchBar!
     
     func didRefresh() {
         self.tableView.reloadData()
         self.refreshControl.endRefreshing()
 
     }
-    override func viewWillAppear(animated: Bool) {
-        loadingAcitivity.startAnimating()
-        loadingAcitivity.hidden = false
-    }
-    override func viewDidAppear(animated: Bool) {
-        loadingAcitivity.stopAnimating()
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadingAcitivity.hidden = true
+        
+        searchbar.delegate = self
         refreshControl = UIRefreshControl()
         self.tableView.addSubview(refreshControl)
         refreshControl.addTarget(self, action: Selector("didRefresh"), forControlEvents: UIControlEvents.ValueChanged)
@@ -49,15 +45,23 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
             delegateQueue:NSOperationQueue.mainQueue()
         )
         
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
+        self.networkView.hidden = true
+        
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
-                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.movies = responseDictionary["results"] as! NSMutableArray
                             self.tableView.reloadData()
+                            MBProgressHUD.hideHUDForView(self.view, animated: true)
                     }
+                }else{
+                    self.networkView.hidden = false
+                    MBProgressHUD.hideHUDForView(self.view, animated: true)
                 }
         });
         task.resume()
@@ -69,23 +73,35 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         if let movies = movies {
-//            if tableView == self.searchDisplayController?.searchResultsTableView {
-//                filteredMovies?.count
-//            }else{
+            if filteredMovies.count != 0{
+                return filteredMovies.count
+                    }else{
             return movies.count
-//            }
+        
+            }
         }else{
             return 0
         }
-        
     }
     
     public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-        let movie = movies![indexPath.row]
+        let movie : NSDictionary
+        if (filteredMovies.count != 0) {
+            movie = filteredMovies[indexPath.row] as! NSDictionary
+        } else {
+            movie = movies![indexPath.row] as! NSDictionary
+        }
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let posterpath = movie["poster_path"] as! String
+        
+        //add fade in fade out
+        cell.posterView.alpha = 0
+        UIView.animateWithDuration(0.3, delay: 0, options: UIViewAnimationOptions.TransitionCurlUp, animations: { () -> Void in
+            cell.posterView.alpha = 1
+            }, completion: nil)
+        
         let urlbase = "http://image.tmdb.org/t/p/w500/"
         
         let imageUrl = NSURL(string: urlbase+posterpath)
@@ -94,6 +110,20 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         cell.title.text = title
         cell.overview.text = overview
         return cell
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if let searchText = searchbar.text{
+        let searchPredicate = NSPredicate(format: "title CONTAINS[c] %@", searchText)
+        let filteredResults = (movies?.filteredArrayUsingPredicate(searchPredicate))!
+            if filteredResults.count != 0 {
+                filteredMovies = filteredResults
+            }else{
+                filteredMovies = []
+            }
+        }
+        tableView.reloadData()
     }
     
     
--- Podfile
+++ Podfile
@@ -5,7 +5,11 @@ use_frameworks!
 
 target 'Flicks' do
 
-pod 'AFNetworking', '~> 2.6'
+pod 'AFNetworking'
+
+pod ‘MBProgressHUD’
+
+
 
 end
 
